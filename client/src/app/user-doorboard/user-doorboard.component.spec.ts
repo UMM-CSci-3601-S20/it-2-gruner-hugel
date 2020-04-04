39497a27e567e3d7bf15f92f17ca743579133b7c
AJ <otten099@morris.umn.edu>
2020-04-04 09:07:04 -0500

    add doorboard component spec

--- client/src/app/user-doorboard/user-doorboard.component.spec.ts
+++ client/src/app/user-doorboard/user-doorboard.component.spec.ts
@@ -1,204 +1,83 @@
-import { HttpClient } from '@angular/common/http';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { ActivatedRoute } from '@angular/router';
+import { ActivatedRouteStub } from '../../testing/activated-route-stub';
+import { MockUserService } from '../../testing/user.service.mock';
 import { User } from '../user';
+import { UserDoorBoardComponent } from './user-doorboard.component';
 import { UserService } from '../user.service';
-import { empty } from 'rxjs';
+import { NotesService } from '../notes.service';
+import { MockNoteService } from 'src/testing/note.service.mock';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('User service: ', () => {
-  // A small collection of test users
-  const testUsers: User[] = [
-    {
-      _id: 'testman_id',
-      name: 'Test Man',
-      officeID: '3168',
-      email: 'tman@gmail.com',
-      building: 'Testing'
-    },
-    {
-      _id: 'spaceman_id',
-      name: 'Space Man',
-      officeID: '9999',
-      email: 'moonrocks@hotmail.com',
-      building: 'The moon'
-    },
-    {
-      _id: 'cowtipper_id',
-      name: 'Cow Tipper',
-      officeID: '4512',
-      email: 'fiendofthebovine@gmail.com',
-      building: 'Some pasture'
-    },
-    {
-      _id: 'alien_id',
-      name: 'Alien',
-      officeID: '9999',
-      email: 'definitelyfriendly@outerspace.com',
-      building: 'The moon'
-    },
-  ];
-  let userService: UserService;
-  // These are used to mock the HTTP requests so that we (a) don't have to
-  // have the server running and (b) we can check exactly which HTTP
-  // requests were made to ensure that we're making the correct requests.
-  let httpClient: HttpClient;
-  let httpTestingController: HttpTestingController;
+describe('UserDoorBoardComponent', () => {
+  let doorBoardComponent: UserDoorBoardComponent;
+  let fixture: ComponentFixture<UserDoorBoardComponent>;
+  const activatedRoute: ActivatedRouteStub = new ActivatedRouteStub();
 
-  beforeEach(() => {
-    // Set up the mock handling of the HTTP requests
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
-    });
-    httpClient = TestBed.inject(HttpClient);
-    httpTestingController = TestBed.inject(HttpTestingController);
-    // Construct an instance of the service with the mock
-    // HTTP client.
-    userService = new UserService(httpClient);
+      imports: [
+        RouterTestingModule,
+        MatCardModule,
+        MatListModule
+      ],
+      declarations: [UserDoorBoardComponent],
+      providers: [
+        { provide: UserService, useValue: new MockUserService() },
+        // { provide: NotesService, useValue: new MockNotesService() },
+        { provide: ActivatedRoute, useValue: activatedRoute }
+      ]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(async(() => {
+    fixture = TestBed.createComponent(UserDoorBoardComponent);
+    doorBoardComponent = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create the component', () => {
+    expect(doorBoardComponent).toBeTruthy();
   });
 
-  afterEach(() => {
-    // After every test, assert that there are no more pending requests.
-    httpTestingController.verify();
+  it('should navigate to a specific user doorboard', () => {
+    const expectedUser: User = MockUserService.testUsers[0];
+    // Setting this should cause anyone subscribing to the paramMap
+    // to update. Our `UserDoorBoardComponent` subscribes to that, so
+    // it should update right away.
+    activatedRoute.setParamMap({ id: expectedUser._id });
+
+    expect(doorBoardComponent.id).toEqual(expectedUser._id);
+    expect(doorBoardComponent.user).toEqual(expectedUser);
   });
 
-  /*it('getUsers() calls api/users', () => {
-    // Assert that the users we get from this call to getUsers()
-    // should be our set of test users. Because we're subscribing
-    // to the result of getUsers(), this won't actually get
-    // checked until the mocked HTTP request 'returns' a response.
-    // This happens when we call req.flush(testUsers) a few lines
-    // down.
-    userService.getUsers().subscribe(
-      users => expect(users).toBe(testUsers)
-    );
+  it('should navigate to correct user when the id parameter changes', () => {
+    let expectedUser: User = MockUserService.testUsers[0];
+    // Setting this should cause anyone subscribing to the paramMap
+    // to update. Our `UserDoorBoardComponent` subscribes to that, so
+    // it should update right away.
+    activatedRoute.setParamMap({ id: expectedUser._id });
 
-    // Specify that (exactly) one request will be made to the specified URL.
-    const req = httpTestingController.expectOne(userService.userUrl);
-    // Check that the request made to that URL was a GET request.
-    expect(req.request.method).toEqual('GET');
-    // Specify the content of the response to that request. This
-    // triggers the subscribe above, which leads to that check
-    // actually being performed.
-    req.flush(testUsers);
+    expect(doorBoardComponent.id).toEqual(expectedUser._id);
+
+    // Changing the paramMap should update the displayed user doorboard.
+    expectedUser = MockUserService.testUsers[1];
+    activatedRoute.setParamMap({ id: expectedUser._id });
+
+    expect(doorBoardComponent.id).toEqual(expectedUser._id);
   });
 
-  it('getUsers() calls api/users with filter parameter \'name\'', () => {
+  it('should have `null` for the user for a bad ID', () => {
+    activatedRoute.setParamMap({ id: 'badID' });
 
-    userService.getUsers({ name: 'Cow Tipper' }).subscribe(
-      users => expect(users).toBe(testUsers)
-    );
-
-    // Specify that (exactly) one request will be made to the specified URL with the name parameter.
-    const req = httpTestingController.expectOne(
-      (request) => request.url.startsWith(userService.userUrl) && request.params.has('name')
-    );
-
-    // Check that the request made to that URL was a GET request.
-    expect(req.request.method).toEqual('GET');
-
-    // Check that the user returned is 'Cow Tipper'
-    expect(req.request.params.get('name')).toEqual('Cow Tipper');
-
-    req.flush(testUsers);
-  });
-
-  it('getUsers() calls api/users with filter parameter \'officeID\'', () => {
-
-    userService.getUsers({ officeID: '9999' }).subscribe(
-      users => expect(users).toBe(testUsers)
-    );
-
-    // Specify that (exactly) one request will be made to the specified URL with the officeID parameter.
-    const req = httpTestingController.expectOne(
-      (request) => request.url.startsWith(userService.userUrl) && request.params.has('officeID')
-    );
-
-    // Check that the request made to that URL was a GET request.
-    expect(req.request.method).toEqual('GET');
-
-    // Check that the role parameter was 'officeID'
-    expect(req.request.params.get('officeID')).toEqual('9999');
-
-    req.flush(testUsers);
-  });
-
-  it('getUsers() calls api/users with filter parameter \'building\'', () => {
-
-    userService.getUsers({ building: 'Some pasture' }).subscribe(
-      users => expect(users).toBe(testUsers)
-    );
-
-    // Specify that (exactly) one request will be made to the specified URL with the officeID parameter.
-    const req = httpTestingController.expectOne(
-      (request) => request.url.startsWith(userService.userUrl) && request.params.has('building')
-    );
-
-    // Check that the request made to that URL was a GET request.
-    expect(req.request.method).toEqual('GET');
-
-    // Check that the role parameter was 'officeID'
-    expect(req.request.params.get('building')).toEqual('Some pasture');
-
-    req.flush(testUsers);
-  });
-
-  it('getUsers() calls api/users with multiple filter parameters', () => {
-    userService.getUsers({ name: 'Alien', building: 'The moon', officeID: '9999' }).subscribe(
-      users => expect(users).toBe(testUsers)
-    );
-
-    // Specify that exactly one request will be made to the specified URL with these parameters.
-    const req = httpTestingController.expectOne(
-      (request) => request.url.startsWith(userService.userUrl) && request.params.has('name')
-        && request.params.has('officeID')
-        && request.params.has('building')
-    );
-    // Should be a GET request
-    expect(req.request.method).toEqual('GET');
-
-    // Should have expected parameters
-    expect(req.request.params.get('name')).toEqual('Alien');
-    expect(req.request.params.get('officeID')).toEqual('9999');
-    expect(req.request.params.get('building')).toEqual('The moon');
-
-    req.flush(testUsers);
-  });*/
-
-  /**
-   * Commented out above because as of now no filters exist, but may later
-   * making these tests useful.
-   */
-
-  it('getUserById() calls api/users/id', () => {
-    // grab an user from our test array
-    const targetUser: User = testUsers[1];
-    // pull the id from that user
-    const targetId: string = targetUser._id;
-    // get that user from the server and check that you got the right one
-    userService.getUserById(targetId).subscribe(
-      user => expect(user).toBe(targetUser)
-    );
-
-    const expectedUrl: string = userService.userUrl + '/' + targetId;
-    const req = httpTestingController.expectOne(expectedUrl);
-    // should be a GET request
-    expect(req.request.method).toEqual('GET');
-    req.flush(targetUser);
-  });
-
-
-  it('addUser() calls api/users/new', () => {
-
-    userService.addUser(testUsers[1]).subscribe(
-      id => expect(id).toBe('testid')
-    );
-
-    const req = httpTestingController.expectOne(userService.userUrl + '/new');
-
-    expect(req.request.method).toEqual('NOTE');
-    expect(req.request.body).toEqual(testUsers[1]);
-
-    req.flush({ id: 'testid' });
+    // If the given ID doesn't map to a user, we expect the service
+    // to return `null`, so we would expect the component's user
+    // to also be `null`.
+    expect(doorBoardComponent.id).toEqual('badID');
+    expect(doorBoardComponent.user).toBeNull();
   });
 
 });
