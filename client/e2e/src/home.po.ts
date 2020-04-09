d1ac066deb8508e998ad8c60a6d4d559fd76189e
Benjamin Burgess <burge337@morris.umn.edu>
2020-04-09 14:31:56 -0500

    Implement e2e testing for user doorboard component
    Progresses issue #17

--- client/e2e/src/home.po.ts
+++ client/e2e/src/home.po.ts
@@ -25,34 +25,4 @@ export class HomePage {
   clickViewDoorBoard(card: ElementFinder) {
     return card.element(by.buttonText('VIEW DOORBOARD')).click();
   }
-
-  // clickAddNewNote() {
-  //   return element(by.className('add-note-fab')).click();
-  // }
-
-  // addNewNote(body: string) {
-  //   element(by.className('add-note-fab')).click();
-  // }
-
-
-  // async getNumberOfNotes(): Promise<number> {
-  //   return await element.all(by.className('note-card')).count();
-  // }
-
-  // async deleteAllNotes() {
-  //   while (await this.getNumberOfNotes() !== 0) {
-  //     this.deleteFirstNote();
-  //   }
-  // }
-
-  // deleteFirstNote() {
-  //   element.all(by.buttonText('delete')).get(0).click();
-  // }
-
-  // editFirstNote() {
-  //   // The button's text says 'create' because that's the name of
-  //   // the material pencil icon. It's not actually a button for
-  //   // creating things.
-  //   element.all(by.buttonText('create')).get(0).click();
-  // }
 }
